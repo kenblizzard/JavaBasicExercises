bab082dcba6dff815df6d1d5011a8b6ac80f4f53
kenblizzard <kenneth.bolico@gmail.com>
2014-03-30 22:52:03 +0800

    master

--- src/com/devkinetics/training/kennethbolico/javabasic1/FileScanner.java
+++ src/com/devkinetics/training/kennethbolico/javabasic1/FileScanner.java
@@ -14,6 +14,7 @@ public class FileScanner {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("hi");
+        System.out.println("hi");
         
         
     }
