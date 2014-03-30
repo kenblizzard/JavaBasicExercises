93c682f0107f575c0a6112652286b7d23d146547
kenblizzard <kenneth.bolico@gmail.com>
2014-03-30 22:42:52 +0800

    Signed-off-by: kenblizzard <kenneth.bolico@gmail.com>

--- src/com/devkinetics/training/kennethbolico/javabasic1/FileScanner.java
+++ src/com/devkinetics/training/kennethbolico/javabasic1/FileScanner.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.devkinetics.training.kennethbolico.javabasic1;
 
 /**
@@ -11,5 +10,8 @@ package com.devkinetics.training.kennethbolico.javabasic1;
  * @author Ken
  */
 public class FileScanner {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
